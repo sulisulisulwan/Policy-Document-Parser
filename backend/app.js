f7a2b8813d2f84ce11170f52b4b5adc3238a0beb
Suliman Tekalli <sulimantekalli@sulimans-mbp.myfiosgateway.com>
2022-01-17 23:24:56 -0500

    Setup repo

---
+++ backend/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.resolve(__dirname + '../client/dist')))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+module.exports = app;
